967ec42cff6a60ca1e839f1681539a339ac521e9
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-04 17:33:17 +0530

    fetch using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import Axios from "axios";
 import { useEffect, useState } from "react";
 function App() {
+  useEffect(() =>{
+    //this is called twice
+    Axios.get("https://catfact.ninja/fact").then((res) =>{
+      setFact(res.data.fact);
+    })
+  }, []);
+
+    //without useEffect it goes infinitely because:
+    /* 
+    it after requests and gets the data,
+    it shows it in the ui..
+    and because of this change the state is again reloaded and hence it again makes the request to fetch the data
+    */
   const [catFact, setFact] = useState("");
   return (
     <div>
