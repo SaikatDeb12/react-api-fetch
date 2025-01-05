cf2e5364ef8a2fc2f768524a437e03d9a81e0c75
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-05 13:55:58 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import Axios from "axios";
+import axios from "axios";
 import { useEffect, useState } from "react";
 function App() {
-  useEffect(() =>{
+  // const fetchData=()=>{
+  // axios.get("http://localhost:8000/api/users/1").then((res) => {
+  //   console.log(res.data);
+  // });
+
+  // }
+  // fetchData();
+
+  // const fetchFact= () =>{
+  //   axios.get("https://catfact.ninja/fact").then((res) =>{
+  //     setFact(res.data.fact);
+  //   });
+  // };
+
+  useEffect(() => {
     //this is called twice
+    axios
+      .patch("http://localhost:8000/api/users/30", {
+        first_name: "saikat",
+        last_name: "deb",
+        email: "saikat@imageshack.us",
+        gender: "male",
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     //after removing the React.StrictMode it runs for only once
-    Axios.get("https://catfact.ninja/fact").then((res) =>{
-      setFact(res.data.fact);
-    })
+    // fetchFact();
   }, []);
 
-    //without useEffect it goes infinitely because:
-    /* 
+  //without useEffect it goes infinitely because:
+  /* 
     it after requests and gets the data,
     it shows it in the ui..
     and because of this change the state is again reloaded and hence it again makes the request to fetch the data
@@ -18,10 +43,10 @@ function App() {
   const [catFact, setFact] = useState("");
   return (
     <div>
-      <button>Get cat facts</button>
-      {catFact};
+      {/* <button onClick={}>Get cat facts</button> */}
+      {/* {catFact}; */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
